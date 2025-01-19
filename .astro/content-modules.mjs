526a4910ef30f26a26d11a999faa7aebb1a2971f
Fanindra Maharana <78368113+imfanindra@users.noreply.github.com>
2025-01-19 14:15:51 +0530

    small changes

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,10 +1,9 @@
 
 export default new Map([
 ["src/content/writing/free-fonts/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Ffree-fonts%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/writing/importance-of-isolation/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Fimportance-of-isolation%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/writing/building-my-sites/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Fbuilding-my-sites%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/writing/2024-recap/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2F2024-recap%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/writing/does-it-matter/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Fdoes-it-matter%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/writing/life-as-introvert/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Flife-as-introvert%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/writing/my-story/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Fmy-story%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/writing/2024-recap/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2F2024-recap%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/content/writing/importance-of-isolation/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Fimportance-of-isolation%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/writing/my-story/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fwriting%2Fmy-story%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
