2151e6362e40d24c9f9ff663f94a1ec73852ab3b
Fanindra Maharana <imfanindra@gmail.com>
2025-02-08 20:58:17 +0530

    coming soon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,7 +9,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Pretendard", ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
         serif: [...defaultTheme.fontFamily.serif]
       },
     },
